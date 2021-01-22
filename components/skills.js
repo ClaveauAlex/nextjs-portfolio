10241f6d40b6169ab7448047a95bc7abd9f80d56
Alexandre Claveau <a.claveau29@gmail.com>
2021-01-22 16:18:56 +0100

    Add data for Skills section

--- components/skills.js
+++ components/skills.js
@@ -2,8 +2,28 @@ import styles from "../styles/Skills.module.css";
 
 export default function Skills() {
     return (
-        <div>
-            <h1>Skills section</h1>
-        </div>
+        <>
+            <h3>Compétences</h3>
+            <div>
+                <h4>Développement</h4>
+                <div>
+                    <p>React</p>
+                    <p>CSS</p>
+                    <p>Node</p>
+                    <p>Express</p>
+                    <p>MongoDB</p>
+                    <p>Git</p>
+                </div>
+            </div>
+            <div>
+                <h4>Design</h4>
+                <div>
+                    <p>Figma</p>
+                    <p>Affinity Designer</p>
+                    <p>Illustrator</p>
+                    <p>Photoshop</p>
+                </div>
+            </div>
+        </>
     );
 }
