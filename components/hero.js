28c01125e2103eabe8f7a1d92b18b8ba04e0ae9f
Alexandre Claveau <a.claveau29@gmail.com>
2021-01-22 15:57:13 +0100

    Elemets for Navbar & HeroContainer

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,25 @@
 import styles from "../styles/Hero.module.css";
+import Image from "next/image";
 
 export default function Hero() {
-    return (
-        <div>
-            <h1>HeroContainer</h1>
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <h1>Je m'appelle Alexandre Claveau</h1>
+        <h3>Développeur Frontend</h3>
+        <p>
+          J'aide les entreprises à créer ou améliorer leur site Web pour
+          convertir plus de visiteurs en clients.
+        </p>
+      </div>
+      <div>
+        <Image
+          src="/me-design.svg"
+          alt="Alexandre Claveau"
+          width={215}
+          height={205}
+        />
+      </div>
+    </>
+  );
 }
--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,21 @@
 import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <Image
+          src="/Logo_bubble.svg"
+          alt="Logo entreprise"
+          width={40}
+          height={40}
+        />
+      </div>
+      <div>
+        <AiOutlineMenu/>
+      </div>
+    </>
+  );
 }
