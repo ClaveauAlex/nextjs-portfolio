eb593e0651249565ba4f9106ed021850536d9005
Alexandre Claveau <a.claveau29@gmail.com>
2021-01-22 17:46:50 +0100

    Add data for footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,46 @@
 import styles from "../styles/Footer.module.css";
+import Link from "next/link";
+import { AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
 
 export default function Footer() {
-    return (
+  const linkedinUrl = "https://www.linkedin.com/in/alexandre-claveau/";
+  const instagramUrl = "https://www.instagram.com/alexandre_claveau.me/";
+
+  return (
+    <>
+      <footer>
         <div>
-            <h1>Footer</h1>
+          <p>Ce site</p>
+          <p>Fait à la main avec React, Nextjs & CSS.</p>
+          <p>© Alexandre Claveau {new Date().getFullYear()}</p>
         </div>
-    );
+        <div>
+          <p>Liens</p>
+          <ul>
+            <li>A propos</li>
+            <li>Compétences</li>
+            <li>Expériences</li>
+            <li>
+              <Link href="/contact">
+                <a>Me contacter</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Trouvez-moi ailleurs</p>
+          <ul>
+            <li>
+              <AiFillLinkedin />
+              <a href={linkedinUrl}>linkedin.fr/claveau</a>
+            </li>
+            <li>
+              <AiFillInstagram />
+              <a href={instagramUrl}>alexandre_claveau.me</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
 }
