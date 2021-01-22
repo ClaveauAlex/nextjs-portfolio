4d1bcffd7c3c8095b5efd93fc27f28ad4962c2e4
Alexandre Claveau <a.claveau29@gmail.com>
2021-01-22 17:31:10 +0100

    Some data for Testimonials section

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,9 +1,27 @@
 import styles from "../styles/Testimonials.module.css";
+import Image from "next/image";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function Testimonials() {
-    return (
-        <div>
-            <h1>Testimonials section</h1>
-        </div>
-    );
+  return (
+    <>
+      <h3>Témoignages</h3>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus mauris
+          diam massa vitae at posuere lorem fringilla. Sit dolor, quis nullam
+          porttitor duis hendrerit.{" "}
+        </p>
+        <IoIosArrowBack />
+        <IoIosArrowForward />
+        <Image
+          src="/testimonial1.svg"
+          alt="Personne Témoignage"
+          width={64}
+          height={64}
+        />
+        <p>Directeur des ventes</p>
+      </div>
+    </>
+  );
 }
