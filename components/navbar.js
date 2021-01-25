4c92cddf3c581b560771d4544941747fdd02ab0b
Alexandre Claveau <a.claveau29@gmail.com>
2021-01-25 15:02:15 +0100

    Add css for Navbar element with responsive

--- components/navbar.js
+++ components/navbar.js
@@ -1,27 +1,39 @@
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image";
-import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar() {
   return (
     <nav className={styles.container}>
-      <div className={styles.logo}>
-        <Image
-          src="/Logo_bubble.svg"
-          alt="Logo entreprise"
-          width={40}
-          height={40}
-        />
-      </div>
+      <Link href="/">
+        <a>
+          <Image
+            src="/Logo_bubble.svg"
+            alt="Logo entreprise"
+            className={styles.logo}
+            width={40}
+            height={40}
+          />
+        </a>
+      </Link>
       <ul className={styles.links}>
-        <li>A propos</li>
-        <li>Compétences</li>
-        <li>Expériences</li>
+        <li>
+          <a href="#">A propos</a>
+        </li>
+        <li>
+          <a href="#">Compétences</a>
+        </li>
+        <li>
+          <a href="#">Expériences</a>
+        </li>
       </ul>
       <div className={styles.menu}>
         <AiOutlineMenu />
       </div>
-      <a className={styles.contact}>Me contacter</a>
+      <Link href="/contact">
+        <a className={styles.contact}>Me contacter</a>
+      </Link>
     </nav>
   );
 }
